b067d07ef6bada2c797a7f3778967af7429f8569
Shane Brennan <brennash@Shanes-MacBook-Pro.local>
2016-05-03 22:55:39 +0100

    First working commit

--- FootieDashboard.py
+++ FootieDashboard.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+from flask import request
+from flask.ext import restful
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	print "Request Received"
-	return render_template('index.html')
+	print request.url 
+	return render_template('index.html', initialSelection=True)
+
+@app.route('/league/<string:league>/<string:season>', methods=['GET'])
+def get_task(league, season):
+	print league, season
+	return render_template('index.html', initialSelection=True)
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050)
