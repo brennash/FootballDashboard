b071898c4e25162abfdb0ef6325a3065e8040d7a
Shane Brennan <brennash@Shanes-MacBook-Pro.local>
2016-05-16 16:23:37 +0100

    Adding details

--- FootieDashboard.py
+++ FootieDashboard.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask import request
 from flask.ext import restful
+from flask.ext.restful import reqparse
 
 app = Flask(__name__)
 
@@ -9,10 +10,16 @@ def index():
 	print request.url 
 	return render_template('index.html', initialSelection=True)
 
-@app.route('/league/<string:league>/<string:season>', methods=['GET'])
-def get_task(league, season):
-	print league, season
-	return render_template('index.html', initialSelection=True)
+@app.route('/api/v1.0/tasks', methods=['GET'])
+def getTask():
+	if request.method == "GET":
+		league = request.args.get('league')
+		season = request.args.get('season')
+		homeTeam = request.args.get('homeTeam')
+		awayTeam = request.args.get('awayTeam')
+		urlPath = request.path
+		
+	return render_template('stats.html')
 
 
 
